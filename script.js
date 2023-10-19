39bfc8395fafa8529138333afc79d7d1a3ffbafd
bahare <pondblue07@gmail.com>
2023-10-19 22:19:59 +0330

    select elements in script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+const generateBtn = document.querySelector('form button')
+const qrCodeBox = document.querySelector('.qr-code')
+const qrInput = document.querySelector('form input')
+const qrImage = document.querySelector('.qr-code img')
