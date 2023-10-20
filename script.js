0d2c0880dc7dcf3de828f08c1e4de0d36d704101
bahare <pondblue07@gmail.com>
2023-10-20 14:10:42 +0330

    used addEventListener for generateBtn in script.js

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ const generateBtn = document.querySelector('form button')
 const qrCodeBox = document.querySelector('.qr-code')
 const qrInput = document.querySelector('form input')
 const qrImage = document.querySelector('.qr-code img')
+
+generateBtn.addEventListener('click' , () => {
+    let qrValue = qrInput.value
+    if(!qrValue){
+        return alert('لطفا یه متن یا آدرس داخل کادر وارد کن!')
+    }
+})
